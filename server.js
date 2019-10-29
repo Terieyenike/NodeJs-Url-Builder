17919713edaf9ab141c71239657fd20ac7886090
Teri Eyenike <teyenike1@gmail.com>
2019-10-29 16:10:54 +0100

    general file settings and update

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/about.html');
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile(__dirname + '/contact.html');
+});
+
+app.listen(9000, () => {
+  console.log('Server is running on port 9000');
+});
